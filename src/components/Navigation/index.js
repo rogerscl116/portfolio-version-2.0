ddc240bd72edf832847cff91faa03040ff3c180b
charity.rogers <rogerscl116@gmail.com>
2021-09-02 14:02:12 -0500

    edit navigation component and css

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,29 +1,19 @@
 import React from 'react';
-import { Nav } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 
 function Navigation() {
 
   return (
 <header>
-  <h2>
-    <a href="/">
-      Charity's Portfolio
-    </a>
-  </h2>
-  <Nav className="justify-content-end" activeKey="/home">
-    <Nav.Item>
-      <Nav.Link href="/about">About me</Nav.Link>
-    </Nav.Item>
-    <Nav.Item>
+  <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="/about">Charity Rogers</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="/about">About Me</Nav.Link>
       <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-    </Nav.Item>
-    <Nav.Item>
       <Nav.Link href="/contact">Contact</Nav.Link>
-    </Nav.Item>
-    <Nav.Item>
       <Nav.Link href="/resume">Resume</Nav.Link>
-    </Nav.Item>
-  </Nav>
+    </Nav>
+  </Navbar>
 </header>
   );
 }
