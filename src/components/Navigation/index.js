1cdaf609e958d2c98329b36e7702dd5d198384e1
charity.rogers <rogerscl116@gmail.com>
2021-09-02 14:30:12 -0500

    add capitalize first letter and validate email helpers

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -5,9 +5,9 @@ function Navigation() {
 
   return (
 <header>
-  <Navbar bg="dark" variant="dark">
+  <Navbar bg="dark" variant="dark" sticky="top">
     <Navbar.Brand href="/about">Charity Rogers</Navbar.Brand>
-    <Nav className="me-auto">
+    <Nav className="me-auto d-flex justify-content-end">
       <Nav.Link href="/about">About Me</Nav.Link>
       <Nav.Link href="/portfolio">Portfolio</Nav.Link>
       <Nav.Link href="/contact">Contact</Nav.Link>
