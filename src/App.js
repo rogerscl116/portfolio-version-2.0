43be6f8b3e35f079a4ee18e92074d4b2c9ebd85c
charity.rogers <rogerscl116@gmail.com>
2021-09-03 15:36:57 -0500

    edit app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from './components/Nav';
 import About from './components/About';
@@ -9,12 +10,16 @@ function App() {
 
   return (
     <div>
-      <Nav />
-      <Header>
-       {/* <main>
-        <About></About>     
-      </main> */}
-      </Header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Header} />
+          <Route path="/about" component={About} />
+          {/* <Route path="/portfolio" component={Portfolio} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/resume" component={Resume} /> */}
+          <Nav />
+        </Switch>
+      </Router>
     </div>
   );
 }
