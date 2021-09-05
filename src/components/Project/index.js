67e2897fed3edbe93ce73c6a2f9b6290a1d1b2ea
charity.rogers <rogerscl116@gmail.com>
2021-09-05 14:38:50 -0500

    add content to portfolio.json

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -12,7 +12,7 @@ function ProjectCards(props) {
               <p className="project-title" id="card-title">{props.name}</p>
               <div id="proj-icons-container">
               <p id="topics">
-                ({props.topics})
+                [{props.topics}]
               </p>
               <a href={props.github}><img src="https://img.icons8.com/fluency/48/000000/github.png" alt="GitHub Repo" id="project-icon"/></a> 
               <a href={props.deploy}><img src="https://img.icons8.com/color/48/000000/launchpad--v1.png" alt="Live Application" id="project-icon"/></a> 
