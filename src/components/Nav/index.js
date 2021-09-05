47adeb44b5ce5dbe3f149c2e406931e8323ef37c
charity.rogers <rogerscl116@gmail.com>
2021-09-05 17:51:13 -0500

    edit footer, nav, resume components and css

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -19,14 +19,14 @@ import Resume from "../Resume";
 function Nav() {
   return (
 <Router>
-<nav className="navbar navbar-expand-lg navbar-light bg-dark">
+<nav className="navbar navbar-expand-lg navbar-light bg-dark sticky-top">
   <Link to="/" className="navbar-brand">Charity Rogers</Link>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <FontAwesomeIcon icon={faBars} style={{ color: 'lightgray'}} />
   </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav ml-auto">
+    <ul className="navbar-nav ms-auto">
       <li className="nav-item active">
         <Link to="/about" className="nav-link">About Me</Link>
       </li>
